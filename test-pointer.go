74f8dcb5a38dab27335de9e436292a464d4f5e3e
Rossi Rahmayani <rossi.rahmayani.rr@gmail.com>
2021-02-15 20:11:17 +0700

    tambah test pointer

---
+++ test-pointer.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+
+	//pass by value
+	var address1 Address = Address{"Sleman", "DI Yogyakarta", "Indonesia"}
+	address2 := address1
+	address2.City = "Bantul"
+
+	fmt.Println(address1) //city tidak berubah
+	fmt.Println(address2)
+	fmt.Println("")
+
+	//pass by reference
+	address3 := &address1
+	address3.City = "Yogyakarta"
+	fmt.Println(address1) //city berubah
+	fmt.Println(address3)
+	fmt.Println("")
+
+	var address4 *Address = &address1
+	address4 = &Address{"Surabaya", "Jawa Timur", "Indonesia"}
+	fmt.Println(address1) //semua tdk berubah
+	fmt.Println(address4)
+	fmt.Println("")
+
+	address5 := &address1
+	*address5 = Address{"Bandung", "Jawa Barat", "Indonesia"}
+	fmt.Println(address1) //semua berubah
+	fmt.Println(address5)
+	fmt.Println("")
+
+	//function new
+	var address6 *Address = new(Address)
+	address6.City = "Padang"
+	fmt.Println(address6)
+
+}
